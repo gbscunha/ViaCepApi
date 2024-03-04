0e956bad2735b6ac471229766cefc345674b3603
gbscunha <gabrielalternativo09@gmail.com>
2024-03-04 10:52:05 -0300

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ViaCepApi
+{
+    public class Class1
+    {
+        private readonly HttpClient _httpClient;
+
+        public Class1() 
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> ConsultarCepAsync(string cep) 
+        {
+            private const string UrlApi = $"https://viacep.com.br/ws/{cep}/json/";
+            HttpResponseMessage response = await _httpClient.GetAsync(UrlApi);
+
+            if (response.IsSuccessStatusCode) 
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return $"Erro na consulta: {response.StatusCode}";
+            }
+        }
+    }
+}
