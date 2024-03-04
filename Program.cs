17b36a4d6137b045c33faa4e2827d01417a15a31
gbscunha <gabrielalternativo09@gmail.com>
2024-03-04 11:01:04 -0300

    Incluindo o App

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaCepApi;
+
+namespace ViaCepApp
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Digite o CEP para consulta: ");
+            string cep = Console.ReadLine();
+
+            var viaCepClient = new Class1();
+            string result = await viaCepClient.ConsultarCepAsync(cep);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
